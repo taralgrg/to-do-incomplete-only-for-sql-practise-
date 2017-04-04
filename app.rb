309cc78d9c57a967e6620daa1ed0e956aded0dc0
Mac <taralgurung@hotmail.com>
2017-04-04 09:50:08 +0800

    fully functional website

--- app.rb
+++ app.rb
@@ -1,7 +1,42 @@
 require("sinatra")
-  require("sinatra/reloader")
-  also_reload("lib/**/*.rb")
-  require("./lib/task")
-  require("pg")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/task")
+require('./lib/list')
+require("pg")
 
-  DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
+
+get("/") do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
+
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list)
+end
+
+post("/tasks") do
+    description = params.fetch("description")
+    list_id = params.fetch("list_id").to_i()
+    @list = List.find(list_id)
+    @task = Task.new({:description => description, :list_id => list_id})
+    @task.save()
+    erb(:task_success)
+  end
